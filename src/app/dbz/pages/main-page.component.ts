ed8458272937c5c8b629491261a714d9ee52733d
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-02-07 17:30:41 -0500

    Angular
    Creacion de interface e implementacion de input para comunicacion entre padre e hijo

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character-interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrl: './main-page.component.css',
 })
 export class MainPageComponent {
-
+  public characters: Character[] = [
+    {
+      name: 'Krilin',
+      power: 1000,
+    },
+    {
+      name: 'Goku',
+      power: 10001,
+    },
+    {
+      name: 'Vegetta',
+      power: 10000,
+    },
+  ];
 }
