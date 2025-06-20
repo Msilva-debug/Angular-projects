fc7b36247add0aa05c2fb4b9eab9ccc115448143
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-06-20 07:43:32 -0500

    Angular
    Creacion de componente para card

---
+++ gifs-app/src/app/gifs/components/gifs-card/gifs-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-card',
+  templateUrl: './gifs-card.component.html',
+  styleUrl: './gifs-card.component.css'
+})
+export class GifsCardComponent {
+  @Input() gif!: Gif;
+}
