cf32d6adf71a82195f51ef4b335938203f1591c7
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-07-04 17:23:18 -0500

    Implementacion de rutas en Angular

--- countryApp/src/app/app.module.ts
+++ countryApp/src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, SharedModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- countryApp/src/app/countries/countries.module.ts
+++ countryApp/src/app/countries/countries.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CountriesRountingModule } from './countries-rounting.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule, CountriesRountingModule
   ]
 })
 export class CountriesModule { }
