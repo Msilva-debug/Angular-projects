eb3e0575c818a3e29c728d3008a97bb4aff27f8a
MatCelis <mateocelis1550@gmail.com>
2025-07-31 15:08:59 -0500

    Angular
    Implementacion de servicios para agregar, editar y eliminar heroe. Implementacion de formulario reactivo para la creacion de heroe.

--- heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-page',
@@ -6,8 +9,28 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class NewPageComponent {
+  constructor(private heroService: HeroesService) {}
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', { nonNullable: true }),
+    publisher: new FormControl<Publisher | null>(Publisher.DCComics),
+    alter_ego: new FormControl<string>(''),
+    first_appearance: new FormControl<string>(''),
+    characters: new FormControl<string>(''),
+    alt_img: new FormControl<string>(''),
+  });
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
+
+  get currentHero(): Hero {
+    return this.heroForm.value as Hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+
+    this.heroService.updateHero(this.currentHero).subscribe({});
+  }
 }
--- heroesApp/src/app/heroes/services/heroes.service.ts
+++ heroesApp/src/app/heroes/services/heroes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { HttpClient } from '@angular/common/http';
 import { environments } from '../../../environments/environments';
@@ -23,5 +23,39 @@ export class HeroesService {
     return this.http
       .get<Hero[]>(`${this.baseUrl}/heroes?q=${term}&_limit=6`)
       .pipe(catchError((error) => of([])));
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero).pipe(
+      tap((newHero) => console.log(`Hero added: ${newHero.id}`)),
+      catchError((error) => {
+        console.error('Error adding hero:', error);
+        return of(hero);
+      })
+    );
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw new Error('Hero ID is required for update');
+    return this.http
+      .patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero)
+      .pipe(
+        tap((updatedHero) => console.log(`Hero updated: ${updatedHero.id}`)),
+        catchError((error) => {
+          console.error('Error updating hero:', error);
+          return of(hero);
+        })
+      );
+  }
+
+  deleteHeroById(id: string): Observable<boolean> {
+    return this.http.delete<boolean>(`${this.baseUrl}/heroes/${id}`).pipe(
+      tap(() => console.log(`Hero deleted: ${id}`)),
+      catchError((error) => {
+        console.error('Error deleting hero:', error);
+        return of(false);
+      }),
+      map(() => true)
+    );
   }
 }
