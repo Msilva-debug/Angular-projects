48ecb02836d22ede1d0bb4ab7ede79e96ad25004
MatCelis <mateocelis1550@gmail.com>
2025-08-01 14:56:39 -0500

    Angular
    Implementacion de auth del usuario

--- heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ heroesApp/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout-page',
@@ -6,9 +9,19 @@ import { Component } from '@angular/core';
   styles: ``,
 })
 export class LayoutPageComponent {
+  constructor(private authService: AuthService, private router: Router) {}
   public sideBarItems = [
     { label: 'Listado', icon: 'label', url: './list' },
     { label: 'Añadir', icon: 'add', url: './new-hero' },
     { label: 'Buscar', icon: 'search', url: './search' },
   ];
+
+  get userName(): User | undefined {
+    console.log('Fetching current user', this.authService.currentUser);
+    return this.authService.currentUser;
+  }
+  public onLogout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
