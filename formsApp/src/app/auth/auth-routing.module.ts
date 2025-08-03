e3cfa15dcad6b2b49d55636078b85083b45d1c9c
Msilva-debug <91212536+Msilva-debug@users.noreply.github.com>
2025-08-03 11:22:10 -0500

    Inicializar la estructura del proyecto de la aplicación de formularios Angular
    Agregue los archivos iniciales del proyecto Angular, incluyendo configuración, enrutamiento, módulos, componentes y recursos compartidos para formsApp. Incluye la configuración de formularios reactivos, autenticación y herramientas básicas del proyecto.

---
+++ formsApp/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './register-page/register-page.component';
+
+const routes: Routes = [
+  { path: 'register', component: RegisterPageComponent },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '**', redirectTo: 'register' },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
