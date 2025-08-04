f9ed201b14ce6a2543adc9dcb8cc40b1e6353f76
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-08-04 17:17:34 -0500

    Angular
    Implementacion de forms en switchespage, implementacion de form en register-page

--- formsApp/src/app/auth/register-page/register-page.component.ts
+++ formsApp/src/app/auth/register-page/register-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
-
+  public formGroup: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.fb.group({});
+  }
 }
--- formsApp/src/app/reactive/switches-page/switches-page.component.ts
+++ formsApp/src/app/reactive/switches-page/switches-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder) {
+    this.resetForm();
+  }
 
+  resetForm() {
+    this.myForm = this.fb.group({
+      gender: ['F', Validators.required],
+      wantNotifications: [true, Validators.requiredTrue],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.resetForm();
+  }
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
 }
