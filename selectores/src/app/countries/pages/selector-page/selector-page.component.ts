d17f92a18c124d2fd9300e98478ed8e99d3b4e81
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-08-06 14:47:19 -0500

    Angular
    Implementacion de interface de country, implementacion de logica para selector dependiendo de respuesta de un selector anterior

--- selectores/src/app/countries/pages/selector-page/selector-page.component.ts
+++ selectores/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import {
+  Country,
+  Region,
+  SmallCountry,
+} from '../../interfaces/country.interface';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'countries-selector-page',
   templateUrl: './selector-page.component.html',
   styles: ``,
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
   public formSelectorPage: FormGroup = new FormGroup({});
-  constructor(private fb: FormBuilder) {
+  public infoCountries: SmallCountry[] = [];
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService
+  ) {
     this.formSelectorPage = this.fb.group({
       region: ['', [Validators.required]],
       country: ['', [Validators.required]],
       borders: ['', [Validators.required]],
     });
+  }
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
+  get regions(): Region[] {
+    return this.countriesService.regions;
+  }
+
+  onRegionChanged(): void {
+    const formControlRegion = this.formSelectorPage.get('region');
+    formControlRegion!.valueChanges
+      .pipe(
+        switchMap((region) =>
+          this.countriesService.getCountriesByRegion(region)
+        ),
+        tap((countries) => (this.infoCountries = countries))
+      )
+      .subscribe();
+  }
+
+  get countries(): SmallCountry[] {
+    return [...this.infoCountries];
   }
 }
---
+++ selectores/src/app/countries/services/countries.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, of, tap } from 'rxjs';
+import { Country, Region, SmallCountry } from '../interfaces/country.interface';
+
+@Injectable({ providedIn: 'root' })
+export class CountriesService {
+  private baseUrl = 'https://restcountries.com/v3.1';
+  private _regions: Region[] = [
+    Region.Africa,
+    Region.Americas,
+    Region.Asia,
+    Region.Europe,
+    Region.Oceania,
+  ];
+
+  constructor(private httpClient: HttpClient) {}
+
+  public getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+
+    const urlGetRegion: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.httpClient.get<Country[]>(`${urlGetRegion}`).pipe(
+      map((countries) =>
+        countries.map((country) => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+    );
+  }
+
+  public get regions(): Region[] {
+    return [...this._regions];
+  }
+}
