932d44a582d783740d1f0a26f75253d6598ad3c3
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-08-11 09:26:24 -0500

    Angular
    Implementacion de listerns para el movimiento del mapa, zoom del mapa y validaciones.

--- mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,19 +1,58 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css',
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap?: ElementRef;
+  public currentLngLat: LngLat = new LngLat(-74.5, 40);
+  public zoom: number = 10;
+  public map?: Map;
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML no existe):';
-    const map = new Map({
-      container: this.divMap?.nativeElement, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+    this.map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: this.currentLngLat,
+      zoom: this.zoom,
     });
+    this.mapListerners();
   }
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+  mapListerners = () => {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+    });
+    this.map.on('moveend', (ev) => {
+      this.currentLngLat = this.map!.getCenter();
+    });
+  };
+  zoomIn = () => {
+    this.map?.zoomIn();
+  };
+
+  zoomOut = () => {
+    this.map?.zoomOut;
+  };
+
+  zoomChanged = (value: string) => {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
+  };
 }
