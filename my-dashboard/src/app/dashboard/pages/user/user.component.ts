ca900010938f50e3010cfe12024bbdf142ad5658
MatCelis <91212536+Msilva-debug@users.noreply.github.com>
2025-09-01 17:55:33 -0500

    Angular
    Implementacion toSignal

--- my-dashboard/src/app/dashboard/pages/user/user.component.ts
+++ my-dashboard/src/app/dashboard/pages/user/user.component.ts
@@ -3,23 +3,36 @@ import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '@services/users.service';
 import { User } from '../../../interfaces/req-response';
 import { JsonPipe } from '@angular/common';
+import { TitleComponent } from '@shared/title/title.component';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [JsonPipe],
-  templateUrl: './user.component.html',
+  imports: [JsonPipe, TitleComponent],
+  template: `<shared-title title-pro="User Details" />
+    @if(user()) {
+    <section>
+      <img [srcset]="user()!.avatar" [alt]="user()!.first_name" />
+      <div>
+        <h3>{{ this.fullName() }}</h3>
+        <p>{{ user()!.email }}</p>
+      </div>
+    </section>
+    }@else {
+    <p>Cargando informacion</p>
+    }`,
   styles: ``,
 })
 export class UserComponent {
-  public service = inject(UsersService);
+  private service = inject(UsersService);
+  private route = inject(ActivatedRoute);
   #user = signal<User | null>(null);
-  public user = computed(() => this.#user());
-  constructor(private route: ActivatedRoute) {
-    const userId = this.route.snapshot.paramMap.get('id');
-    if (!userId) return;
-    this.service.getUserById(userId).subscribe((user) => {
-      this.#user.set(user);
-    });
-  }
+  public user = toSignal(
+    this.route.params.pipe(switchMap(({ id }) => this.service.getUserById(id)))
+  );
+  public fullName = computed(
+    () => `${this.user()!.first_name} ${this.user()!.last_name}`
+  );
 }
